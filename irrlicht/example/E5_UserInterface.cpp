b7d76173d7c614de596ddc9cb6f344ab435eb05c
fdcumt <fdcumt@163.com>
2015-10-10 09:19:58 +0800

    example 5 user interface

--- irrlicht/example/E5_UserInterface.cpp
+++ irrlicht/example/E5_UserInterface.cpp
@@ -34,6 +34,16 @@ enum
 };
 
 
+// set the skin transparency by change the alpha values 
+void setSkinTransparency(s32 alpha, irr::gui::IGUISkin *skin) 
+{
+	for (s32 i=0; i<irr::gui::EGDC_COUNT; ++i) 
+	{
+		video::SColor col = skin->getColor((EGUI_DEFAULT_COLOR)i);
+		col.setAlpha(alpha);
+		skin->setColor((EGUI_DEFAULT_COLOR)i, col);
+	}
+}
 
 
 #endif // end _test_E5_userInterface_
